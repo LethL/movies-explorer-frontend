15395dad9939faca6893832d75b118fe82acc24a
Leth4L <carribean_08@mail.ru>
2022-09-13 14:09:03 +0300

    feat: add 404 page

--- src/components/PageNotFound/PageNotFound.js
+++ src/components/PageNotFound/PageNotFound.js
@@ -1,8 +1,19 @@
+import { Link, useHistory } from "react-router-dom";
 import "./PageNotFound.css";
 
 function PageNotFound() {
-    return(
+    const history = useHistory();
 
+    function goBack() {
+        history.goBack()
+    }
+
+    return(
+        <section className="not-found">
+            <h2 className="not-found__title">404</h2>
+            <p className="not-found__text">Страница не найдена</p>
+            <Link className="not-found__link" onClick={goBack}>Назад</Link>
+        </section>
     );
 }
 
